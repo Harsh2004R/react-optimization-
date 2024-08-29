704275da6e32a28638372932039c8840445d6000
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2024-08-30 06:38:00 +0800

    LC-3 covered

--- rq/src/Components/RQHeros.jsx
+++ rq/src/Components/RQHeros.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import { useQuery } from "react-query"
+import axios from "axios"
+const fetchHeros =()=>{
+  return axios.get("http://localhost:4000/heros")
+}
 function RQHeros() {
+  const { isLoading, data } = useQuery("heros", fetchHeros)
+  if (isLoading) {
+    return (
+      <h2>Loading....</h2>
+    )
+  }
   return (
-    <h1>
-      RQHeros page
-    </h1>
+    <>
+      <h1>
+        RQHeros page
+      </h1>
+      {data?.data.map((el, i) => {
+        return (
+          <div key={i}>
+            {el.name}
+          </div>
+        )
+      })}
+    </>
   )
 }
 
