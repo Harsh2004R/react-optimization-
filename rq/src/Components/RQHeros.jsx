6b816e27dee871383879bc89ccb8098f47741555
Harsh2004R <115461808+Harsh2004R@users.noreply.github.com>
2024-09-12 13:05:17 -0500

    RTQ LC-9 refecth , refetchIntervalInBackground covered

--- rq/src/Components/RQHeros.jsx
+++ rq/src/Components/RQHeros.jsx
@@ -11,6 +11,12 @@ function RQHeros() {
       // staleTime: 10000 // This will prevent of fetch call till given time 
       // refetchOnWindowFocus : true ,false, "always" // it will fetch data when it changed in db if we set it to true or always.
       // retryOnMount : true,false,"always" // this will only triggred when component mounts if true or always...
+      // refetchInterval : true,2000,false  // this will pole(make fetch call again and again) our data after given interval like 2000ms...
+      // refetchIntervalInBackground :true,false,2000 // this will poll our data as background even if we are not on our browser...
+
+
+
+
     }
   )
   console.log(isLoading, isFetching);
